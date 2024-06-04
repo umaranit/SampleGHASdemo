14fa466a8b6dfc111e5f88591d3d710311d01b5d
Umarani <thisisuma@gmail.com>
2024-06-04 19:05:23 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
-
+API_KEY="QWWEW-YYQYQ-QYQYQ-QTQTT-QUUQU";
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
