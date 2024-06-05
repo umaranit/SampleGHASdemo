cba6c2abcd39f2a2f815eeb87c1e1d03f18dbc92
uma <umarani.t@tcs.com>
2024-06-05 09:56:44 +0530

    refactor: update SQLUtility to use parameterized queries for better security

--- Utils/SQLUtility.cs
+++ Utils/SQLUtility.cs
@@ -1,8 +1,11 @@
 using System;
+using System.Data;
 using System.Data.SqlClient;
 
 public class SqlUtility
 {
+   
+    string connectionString;
     public void RunQuery(string userInput)
     {
         string query = "SELECT * FROM Users WHERE Name = '" + userInput + "'";
@@ -16,6 +19,44 @@ public class SqlUtility
             {
                 Console.WriteLine(String.Format("{0}", reader[0]));
             }
+        }
+    }
+     public DataSet GetDataSetByCategory(string catg)
+    {
+        // BAD: the category might have SQL special characters in it
+        using (var connection = new SqlConnection(connectionString))
+        {
+            var query1 = "SELECT ITEM,PRICE FROM PRODUCT WHERE ITEM_CATEGORY='"
+              + catg + "' ORDER BY PRICE";
+            var adapter = new SqlDataAdapter(query1, connection);
+            var result = new DataSet();
+            adapter.Fill(result);
+            return result;
+        }
+
+        // GOOD: use parameters with stored procedures
+        using (var connection = new SqlConnection(connectionString))
+        {
+            var adapter = new SqlDataAdapter("ItemsStoredProcedure", connection);
+            adapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+            var parameter = new SqlParameter("category", catg);
+            adapter.SelectCommand.Parameters.Add(parameter);
+            var result = new DataSet();
+            adapter.Fill(result);
+            return result;
+        }
+
+        // GOOD: use parameters with dynamic SQL
+        using (var connection = new SqlConnection(connectionString))
+        {
+            var query2 = "SELECT ITEM,PRICE FROM PRODUCT WHERE ITEM_CATEGORY="
+              + "@category ORDER BY PRICE";
+            var adapter = new SqlDataAdapter(query2, connection);
+            var parameter = new SqlParameter("category", catg);
+            adapter.SelectCommand.Parameters.Add(parameter);
+            var result = new DataSet();
+            adapter.Fill(result);
+            return result;
         }
     }
 }
