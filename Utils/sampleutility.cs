5e319123df54b59e4b40c4e346ad0c23cbe08525
Umarani <umarani.tejomurtula@microsoft.com>
2024-06-11 16:54:07 +0530

    refactor: Remove hardcoded GitHub personal access token from sampleutility.cs

--- Utils/sampleutility.cs
+++ Utils/sampleutility.cs
@@ -6,6 +6,6 @@ public class SampleUtility
      public static void MethodWithManyParameters(int p1, int p2, int p3, int p4, int p5, int p6, int p7, int p8, int p9, int p10, int p11)
     {
         // Method logic here
-      string github_pat="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";
+      
     }
 }
