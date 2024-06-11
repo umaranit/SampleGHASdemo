c7df86714cf6813d2a0244ee6a0ea13035583901
Umarani <umarani.tejomurtula@microsoft.com>
2024-06-11 16:52:13 +0530

    refactor: update sampleutility.cs to remove hardcoded GitHub personal access token

--- Utils/sampleutility.cs
+++ Utils/sampleutility.cs
@@ -6,7 +6,6 @@ public class SampleUtility
      public static void MethodWithManyParameters(int p1, int p2, int p3, int p4, int p5, int p6, int p7, int p8, int p9, int p10, int p11)
     {
         // Method logic here
-      string aws_access_key_id = Environment.GetEnvironmentVariable("AWS_ACCESS_KEY_ID");
-string aws_secret_access_key = Environment.GetEnvironmentVariable("AWS_SECRET_ACCESS_KEY");
+      string github_pat="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";
     }
 }
