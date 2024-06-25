fe1482f51d9ddf4b28ff47a372ceeb80eebac70f
Umarani <umarani.tejomurtula@microsoft.com>
2024-06-25 12:55:39 +0530

    Add method for reading CSV files in SampleUtility

--- Utils/sampleutility.cs
+++ Utils/sampleutility.cs
@@ -1,11 +1,24 @@
- using System;
-
+using System;
+using System.Collections.Generic;
+using System.IO;
 
 public class SampleUtility
 {
-     public static void MethodWithManyParameters(int p1, int p2, int p3, int p4, int p5, int p6, int p7, int p8, int p9, int p10, int p11)
+    public static void MethodWithManyParameters(int p1, int p2, int p3, int p4, int p5, int p6, int p7, int p8, int p9, int p10, int p11)
     {
         // Method logic here
-      
+    }
+
+    public static List<string> ReadCsvFile(string filePath)
+    {
+        List<string> lines = new List<string>();
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            while (!reader.EndOfStream)
+            {
+                lines.Add(reader.ReadLine());
+            }
+        }
+        return lines;
     }
 }
